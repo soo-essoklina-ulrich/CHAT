962e038a9fff2ea401bb5ca49ee3b3bbc611ad71
soo-essoklina-ulrich <essoklina.soo@ipnetinstitute.com>
2024-07-02 23:00:02 +0000

    comment app

--- app.py
+++ app.py
@@ -44,6 +44,10 @@ def chat_api():
     return jsonify({"response": response})
 
 
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
 # if __name__ == "__main__":
 #     app.run(
 #         host='0.0.0.0',
